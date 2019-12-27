80bdf4660837220147b82f78a028d0b1589e64ae
sam43 <scode43@gmail.com>
2019-12-28 02:41:27 +0600

    SVG interaction controller Js added, working fine

--- app/src/main/java/com/sam43/svginteractiondemo/Repository.kt
+++ app/src/main/java/com/sam43/svginteractiondemo/Repository.kt
@@ -1,6 +1,8 @@
 package com.sam43.svginteractiondemo
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import okhttp3.OkHttpClient
@@ -29,7 +31,7 @@ object Repository {
             .addInterceptor(intercept)
             .build()
         val retrofit = Retrofit.Builder()
-            .baseUrl("") // No base url needed in this project, cz we will download svg image from url
+            .baseUrl("https://svgshare.com") // No base url needed in this project, cz we will download svg image from url
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
@@ -63,3 +65,51 @@ interface APIService {
         @Url fileUrl: String
     ): Call<ResponseBody>
 }
+
+fun Context.toast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
+
+fun getHtmLString(svgString: String) = "<!DOCTYPE HTML>\n" +
+        "<html>\n" +
+        "<head>\n" +
+        "    <meta content=\"width=device-width, initial-scale=1, maximum-scale=10\" name=\"viewport\">\n" +
+        "    <style>\n" +
+        "      body {\n" +
+        "        text-align:center;\n" +
+        "      }\n" +
+        "\n" +
+        "\n" +
+        "    </style>\n" +
+        "</head>\n" +
+        "<body>\n" +
+        "    <h3 id=\"l_value\">WebView</h3>\n" +
+        "<div class=\"container\">\n" +
+        svgString +
+        "</div>\n" +
+        "<script src='index.js' type='text/javascript'></script>" +
+        "</body>\n" +
+        "</html>"
+
+
+fun getHTMLbody(svgString: String) = "<!DOCTYPE HTML>\n" +
+        "<html>\n" +
+        "\n" +
+        "<head>\n" +
+        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=10\">\n" +
+        "    <style>\n" +
+        "        body {\n" +
+        "            text-align: center;\n" +
+        "        }\n" +
+        "    </style>\n" +
+        "</head>\n" +
+        "\n" +
+        "<body>\n" +
+        "    <h3 id=\"l_value\">WebView</h3>\n" +
+        "    <div id=\"div\" class=\"container\">\n" +
+        "\t\n" +
+        svgString +
+        "\n" +
+        "    <script src=\"index.js\"></script>\n" +
+        "</body>\n" +
+        "\n" +
+        "</html>"
